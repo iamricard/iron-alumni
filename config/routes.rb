ac65ee32c2d9594652a0c278301586142c5f87b9
Ricard Sole <ricard.solecasas@gmail.com>
2014-08-25 21:23:04 +0200

    set password on confirmation. closes #37, should merge
    this commit does a lot of stuff, but short story is this: upon
    clicking the confirmation link it prompts the user with a form to
    setup their password.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   root 'front#index'
 
   scope :admin do
-    get '/courses/new',             to: 'admin#new_course',     as: 'new_course_admin'
+    get '/courses/new',              to: 'admin#new_course',     as: 'new_course_admin'
     post '/courses',                 to: 'admin#create_course',  as: 'create_course_admin'
-    get '/courses/:id/members/new', to: 'admin#new_members',    as: 'new_members_admin'
+    get '/courses/:id/members/new',  to: 'admin#new_members',    as: 'new_members_admin'
     post '/courses/:id/members',     to: 'admin#create_members', as: 'create_members_admin'
   end
 
-  devise_for :members, controllers: { registrations: 'custom_registrations' }
+  devise_for :members, controllers: { registrations: 'registrations', confirmations: 'confirmations' }
+  devise_scope :member do
+    put '/confirm', to: 'confirmations#confirm', as: 'create_password'
+  end
   resources :members, only: [:index, :show]
   resources :courses, only: [:index, :show, :create, :update, :update]
   resources :courses do
