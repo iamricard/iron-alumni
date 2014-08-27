96fcf80a1fd90c641865a896c2d7fe32a386b65a
Ricard Sole <ricard.solecasas@gmail.com>
2014-08-27 18:58:00 +0200

    talk happening, me no explainy

--- app/assets/javascripts/app/member/MemberListCtrl.js
+++ app/assets/javascripts/app/member/MemberListCtrl.js
@@ -1,12 +1,22 @@
 angular
   .module('ironalumni')
-  .controller('MemberListCtrl', ['$scope', 'Member', 'Course', '$location', 'Auth', function($scope, Member, Course, $location, Auth) {
+  .controller('MemberListCtrl', ['$scope', '$filter', 'Member', 'Course', '$location', 'Auth', function($scope, $filter, Member, Course, $location, Auth) {
     'use strict';
 
+    $scope.selectedCourse = '';
     $scope.course = { members: Member.query() };
     $scope.courses = Course.query();
-    $scope.filterByCourse = function(course) {
-      if (!course) $scope.course = { members: Member.query() }
-      else $scope.course = Course.get({ id: course.id });
+
+    var formatDate = function(date) {
+      return $filter('date')(date, 'MMM yyyy').toUpperCase();
+    };
+
+    $scope.formatCourse = function(c) {
+      return [c.city.toUpperCase(), c.course_type.toUpperCase(), formatDate(c.start_date)].join(' ');
+    };
+
+    $scope.filterByCourse = function() {
+      if (!$scope.selectedCourse) $scope.course = { members: Member.query() }
+      else $scope.course = Course.get({ id: $scope.selectedCourse.id });
     };
   }]);
